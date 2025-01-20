fd6c267337d512e49c526bb5c754348d184fe3ff
4ngelLugo <lugomiguel372@gmail.com>
2025-01-20 17:32:54 -0500

    Se añadio una imagen aleatoria de un gato con las tres primeras palabras del hecho de la API (cataas)[https://cataas.com]

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Basado en el curso de react de midudev
 APIs:
 
 > Facts Random: [Cat Fact](https://catfact.ninja/fact)
+
 > Imagen random: [Cataas](https://cataas.com/cat/says/hello)
 
 - Recupera un hecho aleatorio de gatos de la primera API
--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,50 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 const FACT_ENDPOINT = 'https://catfact.ninja/fact'
+const CAT_PREFIX_URL = 'https://cataas.com/cat/'
 
 export default function App () {
   const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
 
   useEffect(() => {
     fetch(FACT_ENDPOINT)
-      .then(res => res.json())
-      .then(data => setFact(data.fact))
+      .then((res) => res.json())
+      .then((data) => {
+        const { fact } = data
+        setFact(fact)
+
+        const firstThreeWords = fact.split(' ').slice(0, 3).join(' ')
+        console.log(firstThreeWords)
+
+        fetch(
+          `https://cataas.com/cat/says/${firstThreeWords}?fontSize=100&fontColor=red&json=true`
+        )
+          .then((res) => res.json())
+          .then((response) => {
+            const { _id } = response
+            const url = `${_id}/says/${firstThreeWords}?fontColor=red`
+
+            console.log(url)
+            console.log(response)
+
+            setImageUrl(url)
+          })
+      })
   }, [])
 
   return (
-    <>
+    <main>
       <h1>CAT FACT</h1>
       {fact && <p>{fact}</p>}
-    </>
+      <div>
+        {imageUrl && (
+          <img
+            src={`${CAT_PREFIX_URL}${imageUrl}`}
+            alt='imagen de un gato con las tres primeras palabras del hecho'
+          />
+        )}
+      </div>
+    </main>
   )
 }
